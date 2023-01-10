cfdff720078da44f355e09dc8154d3bd637dae24
Rianne Streef <56265374+RianneStreef@users.noreply.github.com>
2023-01-10 12:30:41 +0100

    tech background

--- src/pages/{ContentfulProduct.slug}.js
+++ src/pages/{ContentfulProduct.slug}.js
@@ -96,175 +96,185 @@ const ProductPage = (props) => {
                 {product.productIntro.productIntro}
               </ReactMarkdown>
             )}
-            {product.techTitle1 && (
-              <div className="tech-description">
-                <h3>Technology</h3>
-                {product.techImg1 && (
+            <div
+              className={`${
+                product.new && product.categorySlug === "clubs"
+                  ? "product-page-tech"
+                  : "product-page"
+              }`}
+            >
+              {product.techTitle1 && (
+                <div className="tech-description">
+                  <h5>TECHNOLOGY</h5>
+                  {product.techImg1 && (
+                    <img
+                      src={product.techImg1.file.url}
+                      className="tech-img"
+                      alt=""
+                    />
+                  )}
+                  <h5>{product.techTitle1}</h5>
+                  <p>{product.techDescription1.techDescription1}</p>
+                </div>
+              )}
+              {product.techTitle2 && (
+                <div className="tech-description">
+                  {product.techImg2 && (
+                    <img
+                      src={product.techImg2.file.url}
+                      className="tech-img"
+                      alt=""
+                    />
+                  )}
+                  <h5>{product.techTitle2}</h5>
+                  <p>{product.techDescription2.techDescription2}</p>
+                </div>
+              )}
+              {product.techTitle3 && (
+                <div className="tech-description">
+                  {product.techImg3 && (
+                    <img
+                      src={product.techImg3.file.url}
+                      className="tech-img"
+                      alt=""
+                    />
+                  )}
+                  <h5>{product.techTitle3}</h5>
+                  <p>{product.techDescription3.techDescription3}</p>
+                </div>
+              )}
+              {product.techTitle4 && (
+                <div className="tech-description">
+                  {product.techImg4 && (
+                    <img
+                      src={product.techImg4.file.url}
+                      className="tech-img"
+                      alt=""
+                    />
+                  )}
+                  <h5>{product.techTitle4}</h5>
+                  <p>{product.techDescription4.techDescription4}</p>
+                </div>
+              )}
+              {product.techTitle5 && (
+                <div className="tech-description">
+                  {product.techImg5 && (
+                    <img
+                      src={product.techImg5.file.url}
+                      className="tech-img"
+                      alt=""
+                    />
+                  )}
+                  <h5>{product.techTitle5}</h5>
+                  <p>{product.techDescription5.techDescription5}</p>
+                </div>
+              )}
+              {product.techTitle6 && (
+                <div className="tech-description">
+                  {product.techImg6 && (
+                    <img
+                      src={product.techImg6.file.url}
+                      className="tech-img"
+                      alt=""
+                    />
+                  )}
+                  <h5>{product.techTitle6}</h5>
+                  <p>{product.techDescription6.techDescription6}</p>
+                </div>
+              )}
+            </div>
+            <div className="product-page">
+              {product.specs && (
+                <div>
+                  <h3>{`Srixon ${product.productName} specs`}</h3>
                   <img
-                    src={product.techImg1.file.url}
-                    className="tech-img"
+                    src={product.specs.file.url}
+                    className="specs-img"
                     alt=""
                   />
-                )}
-                <h5>{product.techTitle1}</h5>
-                <p>{product.techDescription1.techDescription1}</p>
-              </div>
-            )}
-            {product.techTitle2 && (
-              <div className="tech-description">
-                {product.techImg2 && (
-                  <img
-                    src={product.techImg2.file.url}
-                    className="tech-img"
-                    alt=""
-                  />
-                )}
-                <h5>{product.techTitle2}</h5>
-                <p>{product.techDescription2.techDescription2}</p>
-              </div>
-            )}
-            {product.techTitle3 && (
-              <div className="tech-description">
-                {product.techImg3 && (
-                  <img
-                    src={product.techImg3.file.url}
-                    className="tech-img"
-                    alt=""
-                  />
-                )}
-                <h5>{product.techTitle3}</h5>
-                <p>{product.techDescription3.techDescription3}</p>
-              </div>
-            )}
-            {product.techTitle4 && (
-              <div className="tech-description">
-                {product.techImg4 && (
-                  <img
-                    src={product.techImg4.file.url}
-                    className="tech-img"
-                    alt=""
-                  />
-                )}
-                <h5>{product.techTitle4}</h5>
-                <p>{product.techDescription4.techDescription4}</p>
-              </div>
-            )}
-            {product.techTitle5 && (
-              <div className="tech-description">
-                {product.techImg5 && (
-                  <img
-                    src={product.techImg5.file.url}
-                    className="tech-img"
-                    alt=""
-                  />
-                )}
-                <h5>{product.techTitle5}</h5>
-                <p>{product.techDescription5.techDescription5}</p>
-              </div>
-            )}
-            {product.techTitle6 && (
-              <div className="tech-description">
-                {product.techImg6 && (
-                  <img
-                    src={product.techImg6.file.url}
-                    className="tech-img"
-                    alt=""
-                  />
-                )}
-                <h5>{product.techTitle6}</h5>
-                <p>{product.techDescription6.techDescription6}</p>
-              </div>
-            )}
-            {product.specs && (
-              <div>
-                <h3>{`Srixon ${product.productName} specs`}</h3>
-                <img
-                  src={product.specs.file.url}
-                  className="specs-img"
-                  alt=""
-                />
-              </div>
-            )}
-            {product.colors && (
-              <div>
-                <h4>Available colors</h4>
-                <p>{product.colors}</p>
-              </div>
-            )}
-            {product.euro && (
-              <>
-                {product.price1title == null ? (
-                  <h3>{`Srixon ${product.productName} price`}</h3>
-                ) : (
-                  <h3>{`Srixon ${product.price1title} price`}</h3>
-                )}
+                </div>
+              )}
+              {product.colors && (
+                <div>
+                  <h4>Available colors</h4>
+                  <p>{product.colors}</p>
+                </div>
+              )}
+              {product.euro && (
+                <>
+                  {product.price1title == null ? (
+                    <h3>{`Srixon ${product.productName} price`}</h3>
+                  ) : (
+                    <h3>{`Srixon ${product.price1title} price`}</h3>
+                  )}
 
-                <p>
-                  {currencyFormat(product.euro)} &euro;/{" "}
-                  {currencyFormat(product.swiss)} CHF /{" "}
-                  {currencyFormat(product.kroner)} SEK /{" "}
-                  {currencyFormat(product.pound)} &#163;
-                </p>
-              </>
-            )}
-            {product.euro2 && (
-              <>
-                {product.price2title == null ? (
-                  <h3>{`Srixon ${product.productName} price`}</h3>
-                ) : (
-                  <h3>{`Srixon ${product.price2title} price`}</h3>
-                )}
+                  <p>
+                    {currencyFormat(product.euro)} &euro;/{" "}
+                    {currencyFormat(product.swiss)} CHF /{" "}
+                    {currencyFormat(product.kroner)} SEK /{" "}
+                    {currencyFormat(product.pound)} &#163;
+                  </p>
+                </>
+              )}
+              {product.euro2 && (
+                <>
+                  {product.price2title == null ? (
+                    <h3>{`Srixon ${product.productName} price`}</h3>
+                  ) : (
+                    <h3>{`Srixon ${product.price2title} price`}</h3>
+                  )}
 
-                <p>
-                  {currencyFormat(product.euro2)} &euro;/{" "}
-                  {currencyFormat(product.swiss2)} CHF /{" "}
-                  {currencyFormat(product.kroner2)} SEK /{" "}
-                  {currencyFormat(product.pound2)} &#163;
-                </p>
-              </>
-            )}
-            {product.euro3 && (
-              <>
-                {product.price3title == null ? (
-                  <h3>{`Srixon ${product.productName} price`}</h3>
-                ) : (
-                  <h3>{`Srixon ${product.price3title} price`}</h3>
-                )}
+                  <p>
+                    {currencyFormat(product.euro2)} &euro;/{" "}
+                    {currencyFormat(product.swiss2)} CHF /{" "}
+                    {currencyFormat(product.kroner2)} SEK /{" "}
+                    {currencyFormat(product.pound2)} &#163;
+                  </p>
+                </>
+              )}
+              {product.euro3 && (
+                <>
+                  {product.price3title == null ? (
+                    <h3>{`Srixon ${product.productName} price`}</h3>
+                  ) : (
+                    <h3>{`Srixon ${product.price3title} price`}</h3>
+                  )}
 
-                <p>
-                  {currencyFormat(product.euro3)} &euro;/{" "}
-                  {currencyFormat(product.swiss3)} CHF /{" "}
-                  {currencyFormat(product.kroner3)} SEK /{" "}
-                  {currencyFormat(product.pound3)} &#163;
-                </p>
-              </>
-            )}
-            {product.euro4 && (
-              <>
-                {product.price4title == null ? (
-                  <h3>{`Srixon ${product.productName} price`}</h3>
-                ) : (
-                  <h3>{`Srixon ${product.price4title} price`}</h3>
-                )}
+                  <p>
+                    {currencyFormat(product.euro3)} &euro;/{" "}
+                    {currencyFormat(product.swiss3)} CHF /{" "}
+                    {currencyFormat(product.kroner3)} SEK /{" "}
+                    {currencyFormat(product.pound3)} &#163;
+                  </p>
+                </>
+              )}
+              {product.euro4 && (
+                <>
+                  {product.price4title == null ? (
+                    <h3>{`Srixon ${product.productName} price`}</h3>
+                  ) : (
+                    <h3>{`Srixon ${product.price4title} price`}</h3>
+                  )}
 
-                <p>
-                  {currencyFormat(product.euro4)} &euro;/{" "}
-                  {currencyFormat(product.swiss4)} CHF /{" "}
-                  {currencyFormat(product.kroner4)} SEK /{" "}
-                  {currencyFormat(product.pound4)} &#163;
-                </p>
-              </>
-            )}
-            {product.availableWhen && (
-              <p>In store in {product.availableWhen}</p>
-            )}
+                  <p>
+                    {currencyFormat(product.euro4)} &euro;/{" "}
+                    {currencyFormat(product.swiss4)} CHF /{" "}
+                    {currencyFormat(product.kroner4)} SEK /{" "}
+                    {currencyFormat(product.pound4)} &#163;
+                  </p>
+                </>
+              )}
+              {product.availableWhen && (
+                <p>In store in {product.availableWhen}</p>
+              )}
+            </div>
           </div>
         </>
       );
     });
 
   return (
-    <>
+    <div>
       <Helmet>
         <title>Srixon EU & UK Catalogue</title>
         <meta name="robots" content="noindex" />
@@ -278,8 +288,8 @@ const ProductPage = (props) => {
         </Link>
       </div>
       <div className="menu-placeholder" />
-      <div className="padding">{productInfo}</div>
-    </>
+      <div>{productInfo}</div>
+    </div>
   );
 };
 
@@ -294,6 +304,7 @@ export const productQuery = graphql`
         new
         categorySlug
         colors
+        availableWhen
         euro
         euro2
         euro3
