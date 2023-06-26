254602bfa7672cd6d601c0bbb23ed01dcf82cc34
Rianne Streef <56265374+RianneStreef@users.noreply.github.com>
2023-06-26 17:03:42 +0200

    updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://srixon-catalogue.app",
     title: "srixon-catalogue",
   },
   plugins: [
